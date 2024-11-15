abff08decaa998d062598537be123681c0525b48
RaphaelLuizPH <130625114+RaphaelLuizPH@users.noreply.github.com>
2024-11-15 07:28:59 -0300

    Adicionado MENU

---
+++ lib/backupPalavras.h
@@ -0,0 +1,15 @@
+
+#ifndef BACKUPPALAVRAS_H
+#define BACKUPPALAVRAS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <locale.h>
+
+
+void BackupPalavras(FILE *arquivo);
+
+
+#endif //BACKUPPALAVRAS_H
